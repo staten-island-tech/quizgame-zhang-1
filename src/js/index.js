b1874ba87b3b3b8f457ecae22e2da12294e80add
MichilleZ <michillezhang001@gmail.com>
2021-04-12 23:57:33 -0400

    edit

--- src/js/index.js
+++ src/js/index.js
@@ -3,27 +3,18 @@ const quizContainer = document.getElementById("quiz");
 const quizResult = document.getElementById("results");
 const submitButton = document.getElementById("submit")
 
-const theQuestions = [
-    {
-        question: "what is the answer??",
-        answer: {
-            a: a,
-            b: b,
-            c: c,
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "what is the answer??",
-        answer: {
-            a: a,
-            b: b,
-            c: c,
-        },
-        correctAnswer: "a"
-    }
-];
 
 //functions
+//listen for start event
 function startQuiz = (){}
+
 function quizResults = (){}
+
+
+//listen for user answer click
+
+//if anser is ture then update score show correct moal/green check
+//else update score show false modal/ red x
+
+//if question number is less than questions.length show next questions
+//else display results
---
+++ src/js/questions.js
@@ -0,0 +1,21 @@
+//question object 
+const theQuestions = [
+    {
+        question: "what is the answer??",
+        answer: {
+            a: "a",
+            b: "b",
+            c: "c",
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "what is the answer??",
+        answer: {
+            a: a,
+            b: b,
+            c: c,
+        },
+        correctAnswer: "a"
+    }
+];
