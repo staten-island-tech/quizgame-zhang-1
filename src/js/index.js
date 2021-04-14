266dc2ab23e07c97c75e8a65c843b22d6dd420ba
Pamela946 <pzhang2946@gmail.com>
2021-04-13 22:36:01 -0400

    edit

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import { myQuestions } from "./body.js";
+
 //building the quiz
 function buildQuiz() {
   //stores output for HTML
@@ -62,59 +64,6 @@ function showResults() {
 const quizContainer = document.getElementById("quiz");
 const resultsContainer = document.getElementById("results");
 const submitButton = document.getElementById("submit");
-
-//will move this const to body.js later
-const myQuestions = [
-  {
-    question: "What cheese does not really qualify as a cheese?",
-    answers: {
-      a: "Mozzarella",
-      b: "Ricotta",
-      c: "Cheddar",
-      d: "Swiss Cheese",
-    },
-    correctAnswer: "a",
-  },
-  {
-    question:
-      "Where is this line from: I came home on Tuesday and found all of the chairs that I own stacked in a tower in the center of my kitchen.",
-    answers: {
-      a: "a novel",
-      b: "a movie",
-      c: "a poem",
-      d: "a comic strip",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "Are parrots a type of tulip?",
-    answers: {
-      a: "Yes",
-      b: "No",
-    },
-    correctAnswer: "a",
-  },
-  {
-    question: "What cheese is low in lactose?",
-    answers: {
-      a: "Brie",
-      b: "Gorgonzolla",
-      c: "Parmesan",
-      d: "Colby Jack",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "Beans",
-    answers: {
-      a: "Beans",
-      b: "beans",
-      c: "beans?",
-      d: "why",
-    },
-    correctAnswer: "a",
-  },
-];
 
 //build quiz
 buildQuiz();
